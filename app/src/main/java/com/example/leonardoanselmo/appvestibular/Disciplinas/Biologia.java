d439685c6f35590dd8a550307e8670cce98a5639
Mayara Correia <may_tenorio@hotmail.com>
2015-11-23 21:34:56 -0300

    pacote com classes das disciplinas

---
+++ app/src/main/java/com/example/leonardoanselmo/appvestibular/Disciplinas/Biologia.java
@@ -0,0 +1,18 @@
+package com.example.leonardoanselmo.appvestibular.Disciplinas;
+
+import com.example.leonardoanselmo.appvestibular.Questoes;
+
+/**
+ * Created by Mayara on 23/11/2015.
+ */
+public class Biologia {
+    private Questoes[] questoesBiologia;
+
+    public Questoes[] getQuestoesBiologia() {
+        return questoesBiologia;
+    }
+
+    public void setQuestoesBiologia(Questoes[] questoesBiologia) {
+        this.questoesBiologia = questoesBiologia;
+    }
+}
